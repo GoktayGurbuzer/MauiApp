81625d2174a9504111bd18e6f985a342911c85fb
Göktay Gürbüzer <goktaygurbuzer@gmail.com>
2023-09-08 00:16:31 +0300

    Add project files.

---
+++ MauiApp1/ViewModel/MainViewModel.cs
@@ -0,0 +1,54 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+
+namespace MauiApp1.ViewModel;
+
+public partial class MainViewModel : ObservableObject
+{
+    IConnectivity connectivity;
+    public MainViewModel(IConnectivity connectivity)
+    {
+        Items = new ObservableCollection<string>();
+        this.connectivity = connectivity;
+    }
+
+    [ObservableProperty]
+    ObservableCollection<string> items;
+
+    [ObservableProperty]
+    string text;
+
+    [RelayCommand]
+    async void Add()
+    {
+        if (string.IsNullOrWhiteSpace(Text))
+            return;
+
+        if(connectivity.NetworkAccess != NetworkAccess.Internet)
+        {
+            await Shell.Current.DisplayAlert("AAAAAAaa", "İnternet Yok Kardeş", "tm");
+            return;
+        }
+
+        Items.Add(Text);
+        Text = string.Empty;
+    }
+
+    [RelayCommand]
+    void Delete(string s) 
+    {
+        if (Items.Contains(s))
+        {
+            Items.Remove(s);
+        }
+    }
+
+    [RelayCommand]
+    async Task Tap(string s)
+    {
+        await Shell.Current.GoToAsync($"{nameof(DetailPage)}?Text={s}");
+    }
+}
